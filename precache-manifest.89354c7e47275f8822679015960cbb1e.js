65e34055f4cf7ef720b9bd8f0bb02b1d37c56be9
Kaifu Lam <kaifulam@u.washington.edu>
2020-01-20 20:22:34 -0800

    Updates

---
+++ precache-manifest.89354c7e47275f8822679015960cbb1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a57b06d36bb05adc7923c0d4b3871b9",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1fd74c642b59a69037ff",
+    "url": "/monsters-rolodex/static/css/main.6b1e65bc.chunk.css"
+  },
+  {
+    "revision": "60af1f880d4a469bb7e3",
+    "url": "/monsters-rolodex/static/js/2.decf08ee.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.decf08ee.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1fd74c642b59a69037ff",
+    "url": "/monsters-rolodex/static/js/main.f85d8732.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
